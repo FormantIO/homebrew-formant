f2acaf535bf6f4c966963fbe5f4bd796589c98f5
Abraham Dauhajre <abraham@figure.works>
2020-06-15 12:30:20 -0700

    Release v0.75.63

--- Formula/fctl.rb
+++ Formula/fctl.rb
@@ -1,9 +1,9 @@
 class Fctl < Formula
     desc "Formant cli tool"
     homepage "formant.io"
-    url "https://formant.jfrog.io/artifactory/binary/fctl_darwin_amd64_0.75.62.tar.gz"
-    sha256 "1d9a2979ffaad24dbb7f5328c121abafbf4cc2a7cb7c5434c6d7289a67f7ba4e"
-    version "0.75.62"
+    url "https://formant.jfrog.io/artifactory/binary/fctl_darwin_amd64_0.75.63.tar.gz"
+    sha256 "718c6ee8ec16a49d3a38888c852b88dd29b71bb793d6a3d499c99263a1c77207"
+    version "0.75.63"
 
     bottle :unneeded
 
