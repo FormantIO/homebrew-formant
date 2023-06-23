a4c30740ab42481fafbb9bb77d7a99d550b4b0b1
formant-ci <jenkins@formant.io>
2023-06-23 10:29:33 -0700

    Release v1.130.28

--- Formula/fctl.rb
+++ Formula/fctl.rb
@@ -2,7 +2,7 @@ class Fctl < Formula
     desc "Formant cli tool"
     homepage "formant.io"
     url "https://repo.formant.io/artifactory/binary/fctl_darwin_amd64_1.130.28.tar.gz"
-    sha256 "cd450ce3d710c3b420aef5749fa8907455a6cbd5cd21f542f5600f91d84fc9ab"
+    sha256 "1c6936717968e3b145f8b870afd9434ce8ea20c9c7a47bfa49fac4ff2c008fc9"
     version "1.130.28"
 
     def install
