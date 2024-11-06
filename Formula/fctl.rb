b8500a84bd84a3e576f8de0ce27082d30d78b0fd
formant-ci <jenkins@formant.io>
2024-11-05 18:28:24 -0800

    Release v1.195.16

--- Formula/fctl.rb
+++ Formula/fctl.rb
@@ -1,9 +1,9 @@
 class Fctl < Formula
     desc "Formant cli tool"
     homepage "formant.io"
-    url "https://repo.formant.io/artifactory/binary/fctl_darwin_amd64_1.195.14.tar.gz"
-    sha256 "3edb75f7ec3290f25e4b9e40dcc447f9133f0d04db9120babf949a3dee051022"
-    version "1.195.14"
+    url "https://repo.formant.io/artifactory/binary/fctl_darwin_amd64_1.195.16.tar.gz"
+    sha256 "c262425ea5fb91d9edec1800ff3e4e9acbecfa8a3a51f56bc9b5bf43e379d52b"
+    version "1.195.16"
 
     def install
       bin.install "fctl"
